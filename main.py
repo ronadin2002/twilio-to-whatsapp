5abe3a489f720280560b87797026876e19d3b28c
ronadin <ronadin2002@gmail.com>
2024-07-16 15:18:36 +0300

    new website name

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ html_content = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat KING rons web</title>
+    <title>the WA chat mockup</title>
     <style>
         body {
             font-family: Arial, sans-serif;
