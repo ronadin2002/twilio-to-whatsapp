c6265f59ecbfdebc565c29f6db633ed4eba03fef
ronadin <ronadin2002@gmail.com>
2024-07-16 15:52:24 +0300

    -

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ html_content = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>the WA chat</title>
+    <title>the whatsapp scraper</title>
     <style>
         body {
             font-family: Arial, sans-serif;
